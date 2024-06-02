44c910e72d3114fd9de0bdf69910860b8495e06d
eldaniga <eldanyga@gmail.com>
2024-06-02 06:49:25 +0200

    Adding Twitter Component

--- projects/00-hola-mundo/src/main.jsx
+++ projects/00-hola-mundo/src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { App } from './App'
+import "./index.css"
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+
+root.render(
+  <React.Fragment>
+  
+  <App />
+
+  </React.Fragment>
 )
